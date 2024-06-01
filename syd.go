050b698dc0596b9c96e479ecf4cf7263e81add37
inverse-d <user@noemail.org>
2024-06-01 10:03:37 +0200

    Add config read

--- syd.go
+++ syd.go
@@ -1,34 +1,54 @@
 package main
 
-import ( 
+import (
+	"bufio"
 	"fmt"
-	"os"
 	"log"
-	"path/filepath"
+	"os"
 	"strings"
+	"path/filepath"
 )
 
 func main() {
-	content := ImportConfig("~/test")
-	fmt.Println(content)
+	config := "~/.config/syd/syd.conf"
+	content, err := ReadConfig(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for i := 0; i < len(content); i++ {
+		fmt.Println(content[i])
+	}
 }
-func ImportConfig(filename string) string {
+func ReadConfig(filename string) ([]string, error) {
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
 		log.Fatal(err)
 	}
 	if !strings.Contains(filename, "~") {
-		fileContent, err := os.ReadFile(filepath.Join(filename))
+		file, err := os.Open(filepath.Join(filename))
 		if err != nil {
-			log.Fatal(err)	
+			log.Fatal(err)
 		}
-		return string(fileContent)
+		defer file.Close()
+		var dotFiles []string
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			dotFiles = append(dotFiles, scanner.Text())
+		}
+		return dotFiles, scanner.Err()
 	}
-	fileContent, err := os.ReadFile(strings.Replace(filename, "~", homeDir, 1))
-	if err != nil {
-		log.Fatal(err)
-	}
-	return string(fileContent)
+	file, err := os.Open(strings.Replace(filename, "~", homeDir, 1))
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+		var dotFiles []string
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			dotFiles = append(dotFiles, scanner.Text())
+		}
+		return dotFiles, scanner.Err()
+	
 }
 
 func BackupDotfiles() {
